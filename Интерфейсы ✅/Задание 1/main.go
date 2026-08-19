fa89a44a7cead36e075295de2f2485a5afaa3be9
simuchoi <simuchoi@mail.ru>
2026-08-19 14:32:33 +0300

    day 10 - interfaces

---
+++ Интерфейсы ✅/Задание 1/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type car struct {
+	Brand        string  // марка
+	Model        string  // модель
+	Year         int     // год выпуска
+	Color        string  // цвет
+	Power        int     // мощность в л.с.
+	EngineVolume float64 // объём двигателя в литрах
+	FuelType     string  // тип топлива
+	Transmission string  // коробка передач
+	DriveTrain   string  // привод ("FWD" - передний, "RWD" - задний, "AWD" - полный)
+	Doors        int     // количество д
+	Mileage      int     // пробег в км
+	IsNew        bool    // состояние (новая - true) / (б/у - false)
+}
+
+func (c car) Gas() {
+	fmt.Printf("\nGas, %s. Let's go!\n", c.Brand)
+}
+
+func (c car) Brake() {
+	fmt.Printf("\nBrake, %s! Slow down!", c.Brand)
+}
+
+func (c car) getInfo() {
+	fmt.Print("\n---------------------------------------------------------")
+	fmt.Printf("\nHere you are, that's all information about")
+	fmt.Printf("\n%s %s %d i've got for today: ", c.Brand, c.Model, c.Year)
+	fmt.Print("\n---------------------------------------------------------")
+	c.printFields()
+}
+
+func (c car) printFields() {
+	val := reflect.ValueOf(c)
+	typ := reflect.TypeOf(c)
+
+	for i := 3; i < val.NumField(); i++ {
+		fieldName := typ.Field(i).Name
+		fieldValue := val.Field(i)
+		fmt.Printf("\n%s: %v", fieldName, fieldValue)
+	}
+	fmt.Println()
+}
+
+func main() {
+	honda := car{
+		Brand:        "Honda",
+		Model:        "Civic Type R",
+		Year:         2023,
+		Color:        "Yellow",
+		Power:        330,
+		EngineVolume: 2.0,
+		FuelType:     "Benzine",
+		Transmission: "Manual",
+		DriveTrain:   "FWD",
+		Doors:        5,
+		Mileage:      12000,
+		IsNew:        false,
+	}
+
+	honda.Gas()
+	honda.getInfo()
+}
